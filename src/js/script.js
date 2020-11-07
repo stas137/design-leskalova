b7b789f5ab7e11c0d2a459d0e5c8b1f895082221
Stanislav <stasccb@gmail.com>
2020-11-07 15:50:55 +0300

    second

--- src/js/script.js
+++ src/js/script.js
@@ -68,4 +68,22 @@ $(document).ready(function() {
     validateForms('#consultation form');
     validateForms('#order form');
 
+    /* Modal phone mask */
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    /* Submit form */
+    $('form').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
